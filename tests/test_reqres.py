43476c4c7494f2165c5da3c990a01ed0666aedb9
duymai <maiduy082@gmail.com>
2022-10-19 21:11:53 +0700

    first test case

---
+++ tests/test_reqres.py
@@ -0,0 +1,13 @@
+import requests, json
+import pytest
+
+class Test:
+    def test_sample(self):
+        hostname = "https://reqres.in"
+        api = "/api/users?page=2"
+        url = hostname + api
+        res = requests.get(url=url, timeout=2)
+
+        data = res.json()
+
+        assert data["page"] == 2
