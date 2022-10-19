a1b79afe5d7c05c01a03f8804a7f0e1df6063a72
duymai <maiduy082@gmail.com>
2022-10-19 22:19:19 +0700

    added test case for post actopm

--- tests/test_reqres.py
+++ tests/test_reqres.py
@@ -1,13 +1,27 @@
-import requests
+import requests, json
 
 
 class Test:
+
+    host_name = "https://reqres.in"
     def test_sample(self):
-        hostname = "https://reqres.in"
         api = "/api/users?page=2"
-        url = hostname + api
+        url = Test.host_name + api
         res = requests.get(url=url, timeout=2)
 
         data = res.json()
 
         assert data["page"] == 2
+
+    def test_create_new_user(self):
+        api = "/api/users"
+        data = {"name": "Duy Mai",
+                "job": "Tester",
+                "age": 36}
+        url = Test.host_name + api
+        res = requests.post(url=url,
+                            json=data,
+                            timeout=2)
+        assert res.status_code == 201
+        assert res.json()["name"] == "Duy Mai"
+        assert "id" in res.json().keys()
